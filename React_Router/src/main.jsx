efb489ce30f58d811059d7d06e3f17f828f94972
Ehsaan <ehsaanfayaz80@gmail.com>
2024-10-08 19:25:48 +0530

    first commit

---
+++ React_Router/src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+import * as React from "react";
+import * as ReactDom from "react-dom/client";
+import Root from "./routes/root";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Error from "./error-page";
+import Contact from "./routes/contact";
+import { loader as rootLoader, action as rootAction } from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    //root route
+    path: "/",
+    element: <Root />,
+    errorElement: <Error />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+ReactDom.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
